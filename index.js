35259c588aefe81287823e045b0f3f7a84f723d3
dbeattie <darren@fettle.ca>
2019-12-06 17:53:34 +0000

    refactored a piece of code from functional programming to object oriented programming

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// // Arrays to keep track of each task's state
+// const taskTitles = [];
+// const taskComplete = [];
+// const taskDescriptions = [];
+
+// Create a new task by adding to the arrays
+// A new task will be created as incomplete
 function newTask(title, description) {
   const task = {
     title: title,
@@ -5,15 +12,27 @@ function newTask(title, description) {
     complete: false,
 
     logState: function() {
-      console.log(`${task.title} has${task.complete ? " " : " not "}been completed.`);
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
     },
 
     markCompleted: function() {
-      this.complete = true;
+      this.complete = true
     }
   };
   return task;
 }
+
+// // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+// function completeTask(taskIndex) {
+//   task.complete = true;
+// }
+
+// // Print the state of a task to the console in a nice readable way
+// function logTaskState(taskIndex) {
+//   // const title = taskTitles[taskIndex];
+//   // const complete = taskComplete[taskIndex];
+//   console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+// }
 
 // DRIVER CODE BELOW
 
@@ -21,6 +40,47 @@ const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-task1.logState(); //Cat Litter Cleaning Incomplete
-task1.markCompleted();
-task1.logState(); //Cleaning has been completed
+task1.logState();
+task1.markCompleted(task1);
+task1.logState();
+
+// console.log(tasks);
+
+
+// newTask("Clean Cat Litter"); // task 0
+// newTask("Do Laundry"); // task 1
+
+// logTaskState(0); // Clean Cat Litter has not been completed
+// completeTask(0);
+// logTaskState(0); // Clean Cat Litter has been completed
+
+
+
+
+
+// function newTask(title, description) {
+//   const task = {
+//     title: title,
+//     description: description,
+//     complete: false,
+
+//     logState: function() {
+//       console.log(`${task.title} has${task.complete ? " " : " not "}been completed.`);
+//     },
+
+//     markCompleted: function() {
+//       this.complete = true;
+//     }
+//   };
+//   return task;
+// }
+
+// // DRIVER CODE BELOW
+
+// const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+// const task2 = newTask("Do Laundry", "😨");
+// const tasks = [task1, task2];
+
+// task1.logState(); //Cat Litter Cleaning Incomplete
+// task1.markCompleted();
+// task1.logState(); //Cleaning has been completed
